0f000db39a2d4dbd807ec3172a52bb59d3078bb1
Scott Ogilvie <scott-ogilvie@hotmail.com>
2021-08-20 13:22:05 +1200

    Made endpoint

---
+++ PracticeAPI/PlayController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PracticeAPI
+{
+    [Route("Play/")]
+    public class PlayController : Controller
+    {
+        [HttpGet, Route("GetMessage")]
+        public IActionResult GetSuccessMessage()
+        {
+            return Ok("API Accessed!");
+        }
+    }
+}
